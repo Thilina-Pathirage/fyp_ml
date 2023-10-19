1fa6060cadf8a1800ffbaffdef29329c50d3fffb
Thilina-Pathirage <thilinapathirage@gmail.com>
2023-10-19 23:09:17 +0530

    response with recs

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, send_from_directory
 import joblib
-
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import accuracy_score, classification_report
-import joblib
 
 # Load the trained Gradient Boosting model and LabelEncoders
 gb_classifier = joblib.load('./gb_model.pkl')
@@ -14,7 +9,34 @@ le = joblib.load('./label_encoders.pkl')
 
 app = Flask(__name__)
 
-
+# Define recommendations for each stress level
+recommendations_dict = {
+    "Normal": [
+        "Continue maintaining a healthy work-life balance.",
+        "Regular breaks during work can help maintain this state.",
+        "Consider engaging in relaxation exercises or hobbies."
+    ],
+    "Mild": [
+        "Take short breaks during work hours to relax.",
+        "Engage in physical activity; even a short walk can help.",
+        "Consider discussing any work concerns with your supervisor."
+    ],
+    "Moderate": [
+        "Consider speaking to a professional or counselor about stress.",
+        "Engage in regular physical and relaxation activities.",
+        "It may be beneficial to consider vacation or time off."
+    ],
+    "Severe": [
+        "It's crucial to seek professional help or counseling.",
+        "Discuss your workload and concerns with your supervisor.",
+        "Consider taking time off for mental well-being."
+    ],
+    "Extremely Severe": [
+        "Immediate professional intervention is recommended.",
+        "Consider discussing with HR about potential solutions or accommodations.",
+        "Prioritize mental health; taking a break or leave might be necessary."
+    ]
+}
 
 @app.route('/')
 def index():
@@ -32,7 +54,6 @@ def predict():
         
         df = pd.read_csv('./stress_dataset.csv')
         
-        
         # Preprocess the data using the saved LabelEncoders
         for column, encoder in le.items():
             data[column] = encoder.transform([data[column]])[0]
@@ -43,8 +64,14 @@ def predict():
         # Make a prediction using the Gradient Boosting model
         prediction = gb_classifier.predict([input_data])[0]
 
-        # Create a JSON response with the prediction
-        response = {'prediction': prediction}
+        # Get recommendations based on prediction
+        prediction_recommendations = recommendations_dict[prediction]
+
+        # Create a JSON response with the prediction and recommendations
+        response = {
+            'prediction': prediction,
+            'recommendations': prediction_recommendations
+        }
         return jsonify(response), 200
     except Exception as e:
         return str(e), 400
